a7bc466e20190bf225dac360aed04f385f5c92b1
sameer agrawal <sameer agrawal@192.168.0.101>
2020-09-27 16:23:39 +0530

    created packages for controller,service,Dao class and also set the properties in application.properties. First Commit

---
+++ TrainInquiry/src/main/java/com/Rail/TrainInquiry/TrainInquiryApplication.java
@@ -0,0 +1,13 @@
+package com.Rail.TrainInquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainInquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainInquiryApplication.class, args);
+	}
+
+}
---
+++ TrainInquiry/src/test/java/com/Rail/TrainInquiry/TrainInquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rail.TrainInquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainInquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
